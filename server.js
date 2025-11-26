0781f57f470d5ae8f0bf348311644cea0b9887d6
Gerda <example@email.com>
2025-11-26 15:41:27 +0200

    change public to docs

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 const PORT = 3000;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'docs')));
 
 app.get('/', (request, response) => {
     response.sendFile(path.join(__dirname, 'index.html'));
